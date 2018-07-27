c27e71c4e4ff85070ea033ceb9dfe1a73f28c6f9
arielstitt <arielstitt@gmail.com>
2018-07-27 12:22:06 -0400

    controllers for posts model working in postman

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,26 +1,56 @@
 class Api::PostsController < ApplicationController
 
+    
     def index
-        @posts = City.find(params[city_id].posts.order(id: :desc))
-        render json: @posts 
+        @posts = City.find(params[:city_id]).posts.order(id: :desc)
+        render json: @posts
+        
     end
 
     def show
-        @
+       
+        @city_name = City.find(params[:city_id]).name
+        @post = Post.find(params[:id])
+        render json: {
+            content: @post,
+            city_name: @city_name
+        }
+
     end
 
     def create
+        @city = City.find(params[:city_id])
+        @post = @city.posts.create(post_params)
+       
+        render json: {
+            post: @post
+        }
     end
 
     def update
+       @post = Post.find(params[:id])
+       @post.update!(post_params)
+        render json: {
+            post: @post
+        }
     end
 
     def destroy
-    end
-
+        @post = Post.find(params[:id])
+       @post.destroy  
+    end  
+    
     private
 
     def def_post
+        @post = Post.find(params[:id])
     end
+
+
+    def post_params
+        params.require(:post).permit(:title, :image, :content, :city_id)
+    end
+
+    
 
 end
