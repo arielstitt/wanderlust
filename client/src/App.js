ca1d79393cf129f813b4ce12f9540f14ad132a93
arielstitt <arielstitt@gmail.com>
2018-07-29 16:36:26 -0400

    added state to app.js to get sll cities

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios'
+
 
 class App extends Component {
+
+  state = {
+    cities: []
+  }
+
+  componentDidMount() {
+    this.getAllCities()
+  }
+
+  getAllCities = async () => {
+    try {
+      const response = await axios.get("/api/cities");
+      console.log(response.data);
+      this.setState({
+        cities: response.data.cities,
+        cityname: response.data.cityname
+      });
+    } catch (err) {
+      console.log(err);
+      this.setState({ err: err.message });
+    }
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Switch>
+
+            <Route></Route>
+            <Route></Route>
+            <Route></Route>
+
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
