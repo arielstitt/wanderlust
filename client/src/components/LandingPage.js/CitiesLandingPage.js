073d6894db28c3ae9a3ff60d84c787b53eec298d
arielstitt <arielstitt@gmail.com>
2018-07-31 10:38:52 -0400

    mapped through props and city name is in each div. Not styled properly

--- client/src/components/LandingPage.js/CitiesLandingPage.js
+++ client/src/components/LandingPage.js/CitiesLandingPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom'
-import Atlanta from './Atlanta';
 import SanFransisco from './SanFransisco';
 import London from './London'
 import NewYork from './NewYork'
@@ -14,18 +13,29 @@ const CityContainer = styled.div`
     align-items: center;
 `
 
-
+const Atlanta = styled.div`
+width: 100vw;
+height: 75vh;
+background-image: url(https://images.unsplash.com/photo-1453301109223-3e2085a1e69d?ixlib=rb-0.3.5…jEyMDd9&s=f3e07d1…&auto=format&fit=crop&w=634&q=80);
+display: flex;
+justify-content: center;
+align-items: center;
+`
 
 class CitiesLandingPage extends Component {
     render() {
         return (
             <div>
-                {/* refactor these compoenents into a single syty  */}
-                <Atlanta/>
-                <SanFransisco/>
-                <London/>
-                <NewYork/>
-                
+                {/* refactor these compoenents into a single style  */}
+                {this.props.cities.map(city =>{
+                    return (
+                        <Atlanta>
+                            {city.name}
+
+                        </Atlanta>
+                    )
+                })}
+            
             </div>
         );
     }
